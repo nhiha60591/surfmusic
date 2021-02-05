f7f4021c50dfea102ce0ddb8a7f4f85725053c0e
Hien(Hamilton) H.HO <nhiha60591@gmail.com>
2021-02-05 09:15:13 +0700

    Update

---
+++ src/store/modules/user.js
@@ -0,0 +1,43 @@
+const state = {
+  id: null,
+  token: null,
+  name: '',
+  avatar: '',
+  is_admin: false,
+};
+
+const mutations = {
+  SET_ID: (state, id) => {
+    state.id = id;
+  },
+  SET_IS_ADMIN: (state, isAdmin) => {
+    state.is_admin = isAdmin;
+  },
+  SET_TOKEN: (state, token) => {
+    state.token = token;
+  },
+  SET_NAME: (state, name) => {
+    state.name = name;
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar;
+  },
+};
+
+const actions = {
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      commit('SET_TOKEN', '');
+      commit('SET_ROLES', []);
+      resolve();
+    });
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+};
