eae71b7f75388d80c49e837423e602d29d4bbd7d
Hien(Hamilton) H.HO <nhiha60591@gmail.com>
2021-03-10 10:16:30 +0700

    Fix flat button

--- src/components/FlatButton.vue
+++ src/components/FlatButton.vue
@@ -2,7 +2,7 @@
   <div class="flat-btn fixed" :class="flatButtonClass">
     <router-link :to="flatButtonUrl" class="p-4 rounded-full block">
       <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
-        <path id="_Color" data-name=" ↳Color" d="M14,8H8v6H6V8H0V6H6V0H8V6h6Z" fill="rgba(0,0,0,0.87)"/>
+        <path d="M14,8H8v6H6V8H0V6H6V0H8V6h6Z" fill="rgba(0,0,0,0.87)"/>
       </svg>
     </router-link>
   </div>
