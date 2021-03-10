32248da662e9f71c9b7ad9140fce3938f64b5de9
Hien(Hamilton) H.HO <nhiha60591@gmail.com>
2021-03-10 08:13:39 +0700

    Update

--- src/pages/create-playlist.vue
+++ src/pages/create-playlist.vue
@@ -5,10 +5,16 @@
     </div>
     <div class="create-playlist-form pt-10">
       <h1 class="text-center text-white font-bold text-2xl">Create New Playlist</h1>
-      <div class="flex w-full justify-center mt-8">
-        <img src="../assets/icons/audiotrack-white.svg" class="p-5 bg-gray-600 block rounded-lg" width="150" alt="Audio Track" />
+      <div class="flex w-full justify-center mt-8 relative mx-auto" style="max-width: 200px;">
+        <img :src="image" class="p-5 bg-gray-600 block rounded-lg w-full" width="150" alt="Audio Track" />
+        <button class="rounded-full w-12 h-12 flex justify-center items-center absolute bottom-2 right-2 z-20 focus:outline-none" style="background-color: #929292; color: #000;" @click="openFile">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path data-name="Path 144" d="M0,0H24V24H0Z" fill="none"/>
+            <path data-name="Path 145" d="M3,17.25V21H6.75L17.81,9.94,14.06,6.19ZM20.71,7.04a1,1,0,0,0,0-1.41L18.37,3.29a1,1,0,0,0-1.41,0L15.13,5.12l3.75,3.75,1.83-1.83Z" fill="currentColor"/>
+          </svg>
+        </button>
       </div>
-      <input type="file" name="file" ref="file" id="file" style="opacity: 0;">
+      <input type="file" name="file" ref="file" id="file" accept="image/*" style="opacity: 0; width: 1px; height: 1px; overflow: hidden;" @change.prevent="fileChange">
       <div class="my-3">
         <input type="text" class="input-bg p-4 w-full rounded rounded-b-none" placeholder="Playlist Name" />
       </div>
@@ -28,6 +34,19 @@
 import { mapActions } from 'vuex'
 
 export default {
+  data() {
+    return {
+      imageURL: '../assets/icons/audiotrack-white.svg',
+    }
+  },
+  computed: {
+    image() {
+      if (this.imageURL.includes('http')) {
+        return this.imageURL
+      }
+      return require('../assets/icons/audiotrack-white.svg')
+    }
+  },
   methods: {
     ...mapActions('app', ['setShowHeader', 'setShowFlatButton']),
     openFile() {
@@ -38,7 +57,10 @@ export default {
     },
     onCancel() {
       this.$router.push('/')
-    }
+    },
+    fileChange(e) {
+      this.imageURL = window.URL.createObjectURL(e.target.files[0]);
+    },
   },
   created() {
     this.setShowHeader(false)
